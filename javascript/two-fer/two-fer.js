41df6b3c36afabbb064951fee5fa59b7a9764b10
bdbaraban <375@holbertonschool.com>
2019-04-27 21:23:41 +0000

    Set name parameter to undefined as default

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,9 +1,8 @@
-//
-// Given a name, returns a string with the message "One for X, one for me."
-// Where X is the given name.
-//
-
-export const twoFer = name => {
+/**
+ * Returns a string with the message "One for X, one for me."
+ * where X is a given name.
+ */
+export const twoFer = (name = undefined) => {
 	if (name === undefined) {
 		return "One for you, one for me.";
 	}
