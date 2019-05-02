e0df10e2724adb97e26d7ba2d03a9dc987a6245f
bdbaraban <375@holbertonschool.com>
2019-05-02 05:43:33 +0000

    Compact function into one-liner using default variable set to 'you'

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -2,9 +2,4 @@
  * Returns a string with the message "One for X, one for me."
  * where X is a given name.
  */
-export const twoFer = (name = undefined) => {
-	if (name === undefined) {
-		return "One for you, one for me.";
-	}
-	return `One for ${name}, one for me.`;
-}
+export const twoFer = (name = "you") => `One for ${name}, one for me.`;
