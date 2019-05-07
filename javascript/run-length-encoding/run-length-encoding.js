edbe314bc09b7db42e1d14f0afc436f86a587201
bdbaraban <375@holbertonschool.com>
2019-05-07 02:38:15 +0000

    Separate encoding of letter object into helper function

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -4,32 +4,36 @@
  *
  * Return: The run-length encoded string.
  */
-const encode = string => {
-	let letter = {};
-	let l;
-	let encoded = "";
+export const encode = str => {
+  let letter = {};
+  let encoded = "";
 
-	for (let char of string) {
-		if (letter[char] === undefined) {
-			l = Object.keys(letter)[0];
-			if (l !== undefined) {
-				encoded += (letter[l] !== 1) ? `${letter[l]}${l}` : `${l}`;
-				delete letter[l];
-			}
-			letter[char] = 1;
-
-		} else {
-			letter[char] += 1;
-		}
-	}
+  for (let ch of str) {
+    if (letter[ch] === undefined) {
+      encoded = encode_letter(letter, encoded);
+      letter[ch] = 1;
+    } else {
+      letter[ch] += 1;
+    }
+  }
 	
-	l = Object.keys(letter)[0];
-	if (l !== undefined) {
-		encoded += (letter[l] !== 1) ? `${letter[l]}${l}` : `${l}`;
-		delete letter[l];
-	}
+  return encode_letter(letter, encoded);
+}
 
-	return encoded;
+/**
+ * encode_letter - Encode a letter in a run-length encoded string.
+ * letter: An object containing the key/value count of the letter to encode.
+ * encoded: The building run-length encoded string.
+ *
+ * Return: The run-length encoded string with the added encoded letter.
+  */
+const encode_letter = (letter, encoded) => {
+  let l = Object.keys(letter)[0];
+  if (l !== undefined) {
+    encoded += (letter[l] !== 1) ? `${letter[l]}${l}` : `${l}`;
+    delete letter[l];
+  }
+  return encoded;
 }
 
 /**
@@ -38,26 +42,19 @@ const encode = string => {
  *
  * Return: The decoded run-length encoded string.
  */
-const decode = string => {
-	let count = 0;
-	let decoded = "";
+export const decode = string => {
+  let count = 0;
+  let decoded = "";
 
-	for (let char of string) {
-		if (+char >= 1 && +char <= 9) {
-			count = (count * 10) + +char;
-		} else {
-			count = (count === 0) ? 1 : count;
-			while (count > 0) {
-				decoded += char;
-				count--;
-			}
-		}
-	}
+  for (let ch of string) {
+    if (+ch >= 1 && +ch <= 9) {
+      count = (count * 10) + +ch;
+    } else {
+      for (count = (count === 0) ? 1 : count; count > 0; count--) {
+        decoded += ch;
+      }
+    }
+  }
 
-	return decoded;
-}
-
-module.exports = {
-	encode: encode,
-	decode: decode
+  return decoded;
 }
